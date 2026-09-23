1c3dff6c4b443bf8e0789a8d89f7d003ada13caa
ВашеИмя <your_email@example.com>
2026-09-23 14:57:04 +0300

    Сделал редактирование продуктов.

--- catalog/urls.py
+++ catalog/urls.py
@@ -4,8 +4,9 @@ from . import views
 app_name = 'catalog'
 
 urlpatterns = [
-    path('contacts/', views.ContactsTemplateView.as_view(), name ='contact'),
-    path('product_list/', views.ProductListView.as_view(), name ='product_list'),
-    path('product_detail/<int:pk>', views.ProductDetailView.as_view(), name ='product_detail'),
-    path('product_form/', views.ProductCreateView.as_view(), name ='product_create'),
+    path('contacts/', views.ContactsTemplateView.as_view(), name='contact'),
+    path('product_list/', views.ProductListView.as_view(), name='product_list'),
+    path('product_detail/<int:pk>', views.ProductDetailView.as_view(), name='product_detail'),
+    path('product_form/', views.ProductCreateView.as_view(), name='product_create'),
+    path('product_form/<int:pk>', views.ProductUpdateView.as_view(), name='product_update'),
 ]
