e3f008578557944f7db29b5659d55a4005b4bab2
ВашеИмя <your_email@example.com>
2026-09-23 17:11:25 +0300

    Сделал валидность на отрицательную цену продукта.

--- catalog/forms.py
+++ catalog/forms.py
@@ -2,7 +2,6 @@ from django import forms
 from django.core.exceptions import ValidationError
 from catalog.models import Product, Category
 
-
 BAN_WORDS = ['казино', 'криптовалюта', 'крипта',
              'биржа', 'дешево', 'бесплатно', 'обман',
              'полиция', 'радар']
@@ -49,20 +48,29 @@ class ProductForm(forms.ModelForm):
         )
 
     def clean_name(self):
-        name = self.cleaned_data.get('name', '')
-        name_lower = name.lower()
-        for ban_name in BAN_WORDS:
-            if ban_name.lower() in name_lower:
-                raise ValidationError(f'Введено недопустимое слово {ban_name}')
+        name = self.cleaned_data.get('name', None)
+        if name is not None:
+            name_lower = name.lower()
+            for ban_name in BAN_WORDS:
+                if ban_name.lower() in name_lower:
+                    raise ValidationError(f'Введено недопустимое слово {ban_name}')
         return name
 
     def clean_description(self):
-        description = self.cleaned_data.get('description', '')
-        description_lower = description.lower()
-        for ban_name in BAN_WORDS:
-            if ban_name.lower() in description_lower:
-                raise ValidationError(f'Введено недопустимое слово {ban_name}')
+        description = self.cleaned_data.get('description', None)
+        if description is not None:
+            description_lower = description.lower()
+            for ban_name in BAN_WORDS:
+                if ban_name.lower() in description_lower:
+                    raise ValidationError(f'Введено недопустимое слово {ban_name}')
         return description
+
+    def clean_buying_price(self):
+        buying_price = self.cleaned_data.get('buying_price', None)
+        if buying_price is not None and buying_price < 0:
+            raise ValidationError('Цена не может быть отрицательной.')
+        return buying_price
+
 
 class CategoryForm(forms.ModelForm):
     class Meta:
